76e674d3de964c67e7d320f0f89ed94e3acae955
NFA24SCM76P <npatel164@hawk.iit.edu>
2025-03-18 16:24:58 -0500

    analytics added

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "animate.css/animate.compat.css"
+import { Analytics } from "@vercel/analytics/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics/> 
   </React.StrictMode>
 );
 
